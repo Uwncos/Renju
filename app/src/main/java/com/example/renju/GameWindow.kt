2fa903739a6a97f913435be9e92fb789864ebdea
sasha <sasha06122002@gmail.com>
2022-04-21 21:51:03 +0300

    design fixes

--- app/src/main/java/com/example/renju/GameWindow.kt
+++ app/src/main/java/com/example/renju/GameWindow.kt
@@ -2,6 +2,28 @@ package com.example.renju
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ActionMenuView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 class GameWindow : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
